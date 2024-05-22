1ac60256504494068da8d0351668e14febb8732d
Perumalla Venkata Naga Dhansush <perumalladhanush102@gmail.com>
2024-05-22 16:49:22 +0530

    update socket url

--- Front-End/src/context/SocketContext.jsx
+++ Front-End/src/context/SocketContext.jsx
@@ -15,7 +15,7 @@ export const SocketContextProvider = ({ children }) => {
 
   useEffect(() => {
     if (authUser) {
-      const socket = io("https://msgme-7ue2.onrender.com", {
+      const socket = io("https://msg-me.onrender.com", {
         query: {
           userId: authUser._id,
         },
